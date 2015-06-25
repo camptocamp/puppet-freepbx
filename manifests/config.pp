fb42bd28b04056638d3d5dcdf14a02d78086f91c
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-06-25 10:30:19 +0200

    Don't try to auto install freepbx (does not work well)

--- manifests/config.pp
+++ manifests/config.pp
@@ -42,45 +42,45 @@ class freepbx::config {
     password => $freepbx::asterisk_db_pass,
   }
 
-  $mysql_user     = $::mysql::server::options['mysqld']['user']
-  $mysql_password = $::mysql::server::root_password
-
-
-  # We could translate these SQL scripts into puppet receipt, but then this
-  # module would be obsolete every new version of freepbx's SQL scripts
-  exec { 'mysql newinstall.sql':
-    command     => "mysql --user=${mysql_user} --database=asterisk < ${freepbx::asterisk_git_repo_dir}/SQL/newinstall.sql",
-    cwd         => $freepbx::asterisk_git_repo_dir,
-    environment => "MYSQL_PWD=${mysql_password}",
-    unless      => "mysql --user=${mysql_user} asterisk -e \"SELECT * FROM dahdichandids LIMIT 1;\"",
-    path        => $::path,
-  }
-
-  exec { 'mysql cdr_nmysql_table.sql':
-    command     => "mysql --user=${mysql_user} --database=asteriskcdrdb  < ${freepbx::asterisk_git_repo_dir}/SQL/cdr_mysql_table.sql",
-    cwd         => $freepbx::asterisk_git_repo_dir,
-    environment => "MYSQL_PWD=${mysql_password}",
-    unless      => "mysql --user=${mysql_user} asteriskcdrdb -e \"SELECT * FROM cdr LIMIT 1;\"",
-    path        => $::path,
-  }
-
-
-  # Now, install freepbx:
-  ## start asterisk
-  exec { "${freepbx::asterisk_git_repo_dir}/start_asterisk start":
-    cwd    => $freepbx::asterisk_git_repo_dir,
-    unless => 'pgrep asterisk',
-    path   => $::path,
-  }
-
-  exec { "${freepbx::asterisk_git_repo_dir}/install_amp --username=${freepbx::asterisk_db_user} --password=${freepbx::asterisk_db_pass} --webroot ${freepbx::vhost_docroot}":
-    cwd     => $freepbx::asterisk_git_repo_dir,
-    creates => '/usr/local/sbin/amportal',
-    path    => $::path,
-  }
-
-  file { "${freepbx::vhost_docroot}/admin/modules/_cache/":
-    ensure => directory,
-  }
+  #  $mysql_user     = $::mysql::server::options['mysqld']['user']
+  #  $mysql_password = $::mysql::server::root_password
+  #
+  #
+  #  # We could translate these SQL scripts into puppet receipt, but then this
+  #  # module would be obsolete every new version of freepbx's SQL scripts
+  #  exec { 'mysql newinstall.sql':
+  #    command     => "mysql --user=${mysql_user} --database=asterisk < ${freepbx::asterisk_git_repo_dir}/SQL/newinstall.sql",
+  #    cwd         => $freepbx::asterisk_git_repo_dir,
+  #    environment => "MYSQL_PWD=${mysql_password}",
+  #    unless      => "mysql --user=${mysql_user} asterisk -e \"SELECT * FROM dahdichandids LIMIT 1;\"",
+  #    path        => $::path,
+  #  }
+  #
+  #  exec { 'mysql cdr_nmysql_table.sql':
+  #    command     => "mysql --user=${mysql_user} --database=asteriskcdrdb  < ${freepbx::asterisk_git_repo_dir}/SQL/cdr_mysql_table.sql",
+  #    cwd         => $freepbx::asterisk_git_repo_dir,
+  #    environment => "MYSQL_PWD=${mysql_password}",
+  #    unless      => "mysql --user=${mysql_user} asteriskcdrdb -e \"SELECT * FROM cdr LIMIT 1;\"",
+  #    path        => $::path,
+  #  }
+  #
+  #
+  #  # Now, install freepbx:
+  #  ## start asterisk
+  #  exec { "${freepbx::asterisk_git_repo_dir}/start_asterisk start":
+  #    cwd    => $freepbx::asterisk_git_repo_dir,
+  #    unless => 'pgrep asterisk',
+  #    path   => $::path,
+  #  }
+  #
+  #  exec { "${freepbx::asterisk_git_repo_dir}/install_amp --username=${freepbx::asterisk_db_user} --password=${freepbx::asterisk_db_pass} --webroot ${freepbx::vhost_docroot}":
+  #    cwd     => $freepbx::asterisk_git_repo_dir,
+  #    creates => '/usr/local/sbin/amportal',
+  #    path    => $::path,
+  #  }
+  #
+  #  file { "${freepbx::vhost_docroot}/admin/modules/_cache/":
+  #    ensure => directory,
+  #  }
 
 }
